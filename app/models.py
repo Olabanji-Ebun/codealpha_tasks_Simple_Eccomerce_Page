f60a62b38bcd97cfce32024a9495653f87f7af44
Olabanji-Ebun <ayooluwaebun8@gmail.com>
2025-01-20 04:24:07 +0100

    First commit

---
+++ app/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+# Create your models here.
+
+class Product(models.Model):
+    title = models.CharField(max_length=100)
+    selling_pricc = models.FloatField()
+    discounted_price = models.FloatField()
+    description = models.TextField()
+    composition = models.TextField(default='')
+    prodapp = models.TextField(default='')
+    category = models.CharField(choices=CATEGORY_CHOICES, max_length=2)
+    product_image = models.ImageField(upload_to='product')
+    def __str__(self):
+        return self.title
