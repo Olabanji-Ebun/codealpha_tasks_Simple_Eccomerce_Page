16418c1b840b19c10b7f33168d92a67e2c317d61
Olabanji-Ebun <ayooluwaebun8@gmail.com>
2025-01-22 12:30:33 +0100

    Made changes

--- app/views.py
+++ app/views.py
@@ -13,3 +13,8 @@ class CategoryView(View):
         product = Product.objects.filter(category=val)
         title = Product.objects.filter(category=val).values('title')
         return render(request, "app/category.html",locals())
+
+class ProductDetail(View):
+    def get(self, request,pk):
+        product = Product.objects.get(pk=pk)
+        return render(request, "app/productdetail.html", locals())
