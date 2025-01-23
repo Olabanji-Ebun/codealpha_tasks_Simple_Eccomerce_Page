069449720aedd17e675c7cd0e0342b47725973e6
Olabanji-Ebun <ayooluwaebun8@gmail.com>
2025-01-23 14:48:38 +0100

    Created the user registration form

--- app/views.py
+++ app/views.py
@@ -2,7 +2,8 @@ from django.db.models import Count
 from django.shortcuts import render
 from django.views import View
 from . models import Product
-
+from . forms import CustomerRegistrationForm
+from django.contrib import messages
 
 # Create your views here.
 def home(request):
@@ -30,3 +31,18 @@ class ProductDetail(View):
     def get(self, request,pk):
         product = Product.objects.get(pk=pk)
         return render(request, "app/productdetail.html", locals())
+
+class CustomerRegistrationView(View):
+    def get(self, request):
+        form = CustomerRegistrationForm()
+        return render(request, 'app/customerregistration.html',locals())
+    def post(self, request):
+        form = CustomerRegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Congratulations! User Registered Successfully")
+        else:
+            messages.error(request, "Invalid Input Data")
+        return render(request, 'app/customerregistration.html', locals())
+
+
