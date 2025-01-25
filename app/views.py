26ea71aed208563d64071f0df3a1aeb7b59c147b
Olabanji-Ebun <ayooluwaebun8@gmail.com>
2025-01-25 02:50:04 +0100

    Made changes

--- app/views.py
+++ app/views.py
@@ -1,7 +1,7 @@
 from django.db.models import Count
 from django.shortcuts import render
 from django.views import View
-from . models import Product
+from .models import Product, Customer
 from .forms import CustomerRegistrationForm, CustomerProfileForm
 from django.contrib import messages
 
@@ -50,5 +50,17 @@ class ProfileView(View):
         form = CustomerProfileForm()
         return render(request, 'app/profile.html',locals())
     def post(self, request):
+        form = CustomerProfileForm(request.POST)
+        if form.is_valid():
+            user = request.user
+            name = form.cleaned_data['name']
+            locality = form.cleaned_data['locality']
+            city = form.cleaned_data['city']
+            mobile = form.cleaned_data['mobile']
+            state = form.cleaned_data['state']
+            zipcode = form.cleaned_data['zipcode']
+
+            reg = Customer(user=user,name=name,locality=locality,city=city,mobile=mobile,state=state,zipcode=zipcode)
+            reg.save()
         return render(request, 'app/profile.html',locals())
 
