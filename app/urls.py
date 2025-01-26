071a386cb6eb2117cb96890a25685286a8049381
Olabanji-Ebun <ayooluwaebun8@gmail.com>
2025-01-26 23:35:48 +0100

    Made changes

--- app/urls.py
+++ app/urls.py
@@ -28,5 +28,5 @@ urlpatterns = [
    'passwordchangedone'), name='passwordchange'),
     path('passwordchangedone/', auth_view.PasswordChangeDoneView.as_view(template_name='app/'
    'passwordchangedone.html'), name='passwordchangedone'),
-
+    path('logout/', auth_view.LogoutView.as_view(next_page='/login'), name='logout'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- task1/settings.py
+++ task1/settings.py
@@ -123,6 +123,8 @@ MEDIA_ROOT = BASE_DIR / 'media'
 LOGIN_REDIRECT_URL = "/profile/"
 
 
+LOGIN_URL = "login"
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
